3efc10dac073fb08856080b15bf0ab9f89740f50
GuillaumeRiffaud <guillome95r@hotmail.fr>
2021-01-11 12:10:45 +0100

    Début du projet

---
+++ middleware/auth.js
@@ -0,0 +1,17 @@
+const jwt = require('jsonwebtoken');
+
+
+module.exports = (req, res, next) => {
+    try {
+        const token = req.headers.authorization.split(' ')[1];
+        const decodedToken = jwt.verify(token, 'NOT_A_SECRET_ENOUGH_TOKEN_FOR_PROD');
+        const userId = decodedToken.userId;
+        if (req.body.userId && req.body.userId !== userId) {
+            throw 'User ID non valable !';
+        } else {
+            next();
+        }
+    } catch (error) {
+        res.status(401).json({ error: error | 'Requête non authentifiée !' });
+    }
+};
